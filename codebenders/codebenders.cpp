c4aad7ec12c070583506f3419833ab9c5aa42028
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2021-03-03 13:41:37 +0200

    Create structure STUDENT

---
+++ codebenders/codebenders.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum role
+{
+    scrumTrainor,
+    QAEngineer,
+    developerBackend,
+    developerFrontend
+};
+
+struct STUDENT
+{
+    string name;
+    string surname;
+    int schoolYear;
+    char className;
+    role teamRole;
+    string email;
+};
+
+int main()
+{
+   
+
+}
