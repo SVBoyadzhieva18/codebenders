c4f5bce65071e1f5c26427db815c70e383f2471b
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2021-03-13 23:04:58 +0200

    Add functions for searching for students by a certain criteria

--- codebenders/codebenders.cpp
+++ codebenders/codebenders.cpp
@@ -48,7 +48,7 @@ struct STUDENT
     string name;
     string surname;
     int schoolYear;
-    char className;
+    char className = ' ';
     role teamRole = role::scrumTrainer;
     string email;
 };
@@ -60,6 +60,7 @@ struct TEAM
     DATE dateOfSetUp;
     STUDENT teammates[4];
     status teamStatus = status::notArchived;
+    int numOfStudents;
 };
 
 struct TEACHER
@@ -68,6 +69,7 @@ struct TEACHER
     string surname;
     TEAM teams[5];
     string email;
+    int numOfTeams;
 };
 
 struct SCHOOL
@@ -81,6 +83,8 @@ struct SCHOOL
 };
 
 
+/*=============================================================================*/
+
 void initStudents(STUDENT* students, int& index)
 {
     students[0] = { "Maria", "Georgieva", 10, 'A', role::scrumTrainer, "mariaG@abv.bg" };
@@ -92,7 +96,7 @@ void initStudents(STUDENT* students, int& index)
     students[6] = { "Aneliya", "Shishmanova", 10, 'V', role::developerBackend, "aneliq04@gamil.com" };
     students[7] = { "Stoyan", "Dobrev", 8, 'G', role::QAEngineer, "sDobrev@abv.bg" };
     students[8] = { "Galin", "Georgiev", 8, 'G', role::scrumTrainer, "gggeorgiev@yahoo.org" };
-    students[9] = { "Galena", "Hadzhieva", 9, 'V', role::developerBackend, "geymarkata@gmail.com"};
+    students[9] = { "Galena", "Hadzhieva", 9, 'V', role::developerBackend, "geymarkata@gmail.com" };
     students[10] = { "Didi", "Popova", 10, 'A', role::QAEngineer, "Dida_D@abv.bg" };
     students[11] = { "Radomir", "Liliev", 9, 'B', role::scrumTrainer, "RadoGashtite@gmail.com" };
 
@@ -102,12 +106,12 @@ void initStudents(STUDENT* students, int& index)
 
 void showStudent(STUDENT* students, int index)
 {
-    cout <<"Student ID: " << index + 1<<endl;
-    cout << "  Name: "<<students[index].name << " " << students[index].surname<<endl;
+    cout << "Student ID: " << index + 1 << endl;
+    cout << "  Name: " << students[index].name << " " << students[index].surname << endl;
     cout << "  Class: " << students[index].schoolYear << " " << students[index].className << endl;
     cout << "  Role: ";
-    if (students[index].teamRole==0)
-        cout<<"Scrum Trainer"<< endl;
+    if (students[index].teamRole == 0)
+        cout << "Scrum Trainer" << endl;
     else if (students[index].teamRole == 1)
         cout << "QA Engineer" << endl;
     else if (students[index].teamRole == 2)
@@ -120,7 +124,7 @@ void showStudent(STUDENT* students, int index)
 void showStudents(STUDENT* students, int index)
 {
 
-    cout << "List of students: " << endl<<endl;
+    cout << "List of students: " << endl << endl;
     for (int i = 0; i < index; i++)
     {
         showStudent(students, i);
@@ -140,14 +144,14 @@ void enterStudent(STUDENT* students, int& index)
     cout << " Class (number and letter): ";
     cin >> students[index].schoolYear >> students[index].className;
 
-    cout << " Role in team: "<<endl;
+    cout << " Role in team: " << endl;
     cout << "  1. Scrum Trainer" << endl;
     cout << "  2. QA Engineer" << endl;
     cout << "  3. Backend Developer" << endl;
     cout << "  4. Frontend Developer" << endl;
     cout << "Enter choice: ";
     cin >> choice;
-    students[index].teamRole = role(choice-1);
+    students[index].teamRole = role(choice - 1);
 
     cout << "E-mail: ";
     cin >> students[index].email;
@@ -157,7 +161,7 @@ void enterStudent(STUDENT* students, int& index)
 
 void deleteStudent(STUDENT* students, int& index, int position)
 {
-    for (int i = position; i < index-1; i++)
+    for (int i = position; i < index - 1; i++)
     {
         students[i] = students[i + 1];
     }
@@ -186,24 +190,24 @@ void updateStudentClass(STUDENT* students, int position)
 void updateStudentRole(STUDENT* students, int position)
 {
     int choice;
-    cout << "Edit the role of the selected student (choose from the options listed below): " << endl<<endl;
+    cout << "Edit the role of the selected student (choose from the options listed below): " << endl << endl;
 
     cout << " 1. Scrum Trainer" << endl;
     cout << " 2. QA Engineer" << endl;
     cout << " 3. Backend Developer" << endl;
-    cout << " 4. Frontend Developer" << endl<<endl;
+    cout << " 4. Frontend Developer" << endl << endl;
 
     cout << "Enter your choice: ";
     cin >> choice;
 
     if (choice == 1)
-        students[position].teamRole == role::scrumTrainer;
+        students[position].teamRole = role::scrumTrainer;
     else if (choice == 2)
-        students[position].teamRole == role::QAEngineer;
+        students[position].teamRole = role::QAEngineer;
     else if (choice == 3)
-        students[position].teamRole == role::developerBackend;
+        students[position].teamRole = role::developerBackend;
     else
-        students[position].teamRole == role::developerFrontend;
+        students[position].teamRole = role::developerFrontend;
 }
 
 void updateStudentEmail(STUDENT* students, int position)
@@ -213,27 +217,101 @@ void updateStudentEmail(STUDENT* students, int position)
     cin >> students[position].email;
 }
 
+void searchStudentByName(STUDENT* students, int& index)
+{
+    string name, surname;
+    cout << "Enter the first and last name of the student you are looking for: ";
+    cout << " First name: ";
+    cin >> name;
+    cout << " Last name: ";
+    cin >> surname;
+
+    cout << endl;
+
+    for (int i = 0; i < index; i++)
+    {
+        if (students[i].name == name and students[i].surname == surname)
+        {
+            showStudent(students, i);
+        }
+    }
+}
+
+void seacrhStudentsByClass(STUDENT* students, int& index)
+{
+    int schoolYear;
+    char className;
+    cout << "Show all the students from this class: ";
+    cout << " School year: ";
+    cin >> schoolYear;
+    cout << " Class name: ";
+    cin >> className;
+
+    cout << endl;
+
+    for (int i = 0; i < index; i++)
+    {
+        if (students[i].schoolYear == schoolYear and students[i].className == className)
+        {
+            showStudent(students, i);
+        }
+    }
+}
+
+void searchStudentsByRole(STUDENT* students, int& index)
+{
+    int choice;
+    role option;
+    cout << "Students in which role would you like to see?" << endl;
+    cout << "1. Scrum Trainer" << endl;
+    cout << "2. QA Engineer" << endl;
+    cout << "3. Backend Developer" << endl;
+    cout << "4. Frontend Developer" << endl<<endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    cout << endl;
+
+    if (choice == 1)
+        option = role::scrumTrainer;
+    else if (choice == 2)
+        option = role::QAEngineer;
+    else if (choice == 3)
+        option = role::developerBackend;
+    else
+        option = role::developerFrontend;
+
+    for (int i = 0; i < index; i++)
+    {
+        if (students[i].teamRole == option)
+        {
+            showStudent(students, i);
+        }
+    }
+}
+
+/*===========================================================================*/
 
 void initTeams(STUDENT* students, int& index, TEAM* teams, int& t_index)
 {
-    teams[0] = { "Vikings", "We are here to win!", {10,2,2021}, {students[2], students[4], students[10]}, status::notActive };
-    teams[1] = { "We Showed Up", "Hoping for the best with minimum effort!", {13,2,2021}, {students[7], students[9], students[11]}, status::inUse };
-    teams[2] = { "Cereal Killers", "Trying our best", {20, 2, 2021},  {students[0], students[1], students[5]}, status::notArchived };
+    teams[0] = { "Vikings", "We are here to win!", {10,2,2021}, {students[2], students[4], students[10]}, status::notActive, 3 };
+    teams[1] = { "We Showed Up", "Hoping for the best with minimum effort!", {13,2,2021}, {students[7], students[9], students[11]}, status::inUse, 3 };
+    teams[2] = { "Cereal Killers", "Trying our best", {20, 2, 2021},  {students[0], students[1], students[5]}, status::notArchived, 3 };
 
     t_index = 3;
 }
 
-void showTeam(STUDENT* students, TEAM* teams, int& t_index, int teamMembers)
+void showTeam(STUDENT* students, TEAM* teams, int& t_index)
 {
-    cout << "Name: " << teams[t_index].name<<endl;
+    cout << "Name: " << teams[t_index].name << endl;
     cout << "Description: " << teams[t_index].description << endl;
-    cout << "Date of set-up: " << teams[t_index].dateOfSetUp.day << "/" 
+    cout << "Date of set-up: " << teams[t_index].dateOfSetUp.day << "/"
         << teams[t_index].dateOfSetUp.month << "/" << teams[t_index].dateOfSetUp.year << endl;
     cout << "Teammates: " << endl;
-    for (int i = 0; i < teamMembers; i++)
+
+    for (int i = 0; i < teams[t_index].numOfStudents; i++)
     {
         cout << " " << i + 1 << ". " << teams[t_index].teammates[i].name << " " << teams[t_index].teammates[i].surname << " - ";
-       
+
         if (teams[t_index].teammates[i].teamRole == 0)
             cout << "Scrum Traner" << endl;
         else if (teams[t_index].teammates[i].teamRole == 1)
@@ -244,7 +322,7 @@ void showTeam(STUDENT* students, TEAM* teams, int& t_index, int teamMembers)
             cout << "Frontend Developer" << endl;
     }
 
-        cout << "Status: ";
+    cout << "Status: ";
     if (teams[t_index].teamStatus == 0)
         cout << "In use";
     else if (teams[t_index].teamStatus == 1)
@@ -253,30 +331,30 @@ void showTeam(STUDENT* students, TEAM* teams, int& t_index, int teamMembers)
         cout << "Not archived";
 }
 
-void showTeams(STUDENT* students, TEAM* teams, int& t_index, int teamMembers)
+void showTeams(STUDENT* students, TEAM* teams, int& t_index)
 {
     cout << "List of teams: " << endl << endl;
     for (int i = 0; i < t_index; i++)
     {
-        showTeam(students, teams, i, teamMembers);
+        showTeam(students, teams, i);
         cout << endl << endl;
     }
 }
 
-void enterTeam(STUDENT* students,  TEAM* teams, int& t_index)
+void enterTeam(STUDENT* students, TEAM* teams, int& t_index)
 {
-    int number,id;
+    int number, id;
 
     cout << "Enter info about a new team:" << endl;
 
     cout << "Name of the team: ";
-    getline (cin, teams[t_index].name);
-    
+    getline(cin, teams[t_index].name);
+
 
     cout << "Description: ";
-    getline (cin,teams[t_index].description);
+    getline(cin, teams[t_index].description);
 
-    cout << "Date of set up: "<<endl;
+    cout << "Date of set up: " << endl;
     cout << " Day: ";
     cin >> teams[t_index].dateOfSetUp.day;
     cout << " Month: ";
@@ -287,12 +365,14 @@ void enterTeam(STUDENT* students,  TEAM* teams, int& t_index)
     cout << "Number of members: ";
     cin >> number;
 
-    cout << "Enter the ID numbers of the members of the team: "<<endl;
+    teams[t_index].numOfStudents = number;
+
+    cout << "Enter the ID numbers of the members of the team: " << endl;
     for (int i = 0; i < number; i++)
     {
         cout << " " << i + 1 << ". ";
         cin >> id;
-        teams[t_index].teammates[i] = students[id-1];
+        teams[t_index].teammates[i] = students[id - 1];
     }
 
     t_index++;
@@ -312,14 +392,14 @@ void updateTeamName(TEAM* teams, int pos)
 {
     cout << "Edit the name of the selected team:" << endl;
     cout << " New team name: ";
-    getline (cin, teams[pos].name);
+    getline(cin, teams[pos].name);
 }
 
 void updateTeamDescription(TEAM* teams, int pos)
 {
     cout << "Edit the desciption of the selected team: " << endl;
     cout << " New team description: ";
-    getline (cin, teams[pos].description);
+    getline(cin, teams[pos].description);
 }
 
 void updateTeamStatus(TEAM* teams, int pos)
@@ -328,52 +408,76 @@ void updateTeamStatus(TEAM* teams, int pos)
     cout << "Edit the status of the selected team (choose from the options listed below): " << endl;
     cout << "1. In use" << endl;
     cout << "2. Not active" << endl;
-    cout << "3. Not archived" << endl<<endl;
+    cout << "3. Not archived" << endl << endl;
 
     cout << "Enter your choice: ";
     cin >> choice;
 
     if (choice == 1)
-        teams[pos].teamStatus == status::inUse;
+        teams[pos].teamStatus = status::inUse;
     else if (choice == 2)
-        teams[pos].teamStatus == status::notActive;
+        teams[pos].teamStatus = status::notActive;
     else
-        teams[pos].teamStatus == status::notArchived;
+        teams[pos].teamStatus = status::notArchived;
 }
 
+void addStudentToTeam(STUDENT* students, TEAM* teams, int position)
+{
+    int id;
+    cout << "Enter the ID number of the student you want to be added to the selected team: ";
+    cin >> id;
+
+    teams[position].teammates[teams[position].numOfStudents] = students[id - 1];
+    teams[position].numOfStudents++;
+}
+
+void removeStudentFromTeam(STUDENT* students, TEAM* teams, int t_index, int position)
+{
+    int numberOfStudents = teams[t_index].numOfStudents;
+
+    for (int i = position; i < numberOfStudents - 1; i++)
+    {
+        teams[t_index].teammates[i] = teams[t_index].teammates[i + 1];
+    }
+
+    teams[t_index].numOfStudents--;
+}
+
+/*==========================================================================*/
 
 void initTeachers(TEAM* teams, TEACHER* teachers, int& tch_index)
 {
-    teachers[0] = { "Ivelina", "Staykova", {teams[0], teams[1]}, "iStaikova@abv.bg" };
-    teachers[1] = { "Yordan", "Kamenov", {teams[2]}, "yordanKA@gmail.com" };
+    teachers[0] = { "Ivelina", "Staykova", {teams[0], teams[1]}, "iStaikova@abv.bg", 2 };
+    teachers[1] = { "Yordan", "Kamenov", {teams[2]}, "yordanKA@gmail.com", 1 };
 
     tch_index = 2;
 }
 
-void showTeacher(TEAM* teams, TEACHER* teachers, int& tch_index, int numberTeams)
+void showTeacher(TEAM* teams, TEACHER* teachers, int& tch_index)
 {
-    cout << "First and last name: "<<teachers[tch_index].name<<" "<< teachers[tch_index].surname<<endl;
-    cout << "Teams they assist: "<<endl;
-    for (int i = 0; i < numberTeams; i++)
+    int numOfTeams = teachers[tch_index].numOfTeams;
+    cout << "First and last name: " << teachers[tch_index].name << " " << teachers[tch_index].surname << endl;
+    cout << "Teams they assist: " << endl;
+    for (int i = 0; i < numOfTeams; i++)
     {
         cout << " " << i + 1 << ". " << teachers[tch_index].teams[i].name << endl;
     }
     cout << "E-mail: " << teachers[tch_index].email << endl;
 }
 
-void showTeachers(TEAM* teams, TEACHER* teachers, int& tch_index, int numberTeams)
+void showTeachers(TEAM* teams, TEACHER* teachers, int& tch_index)
 {
     cout << "List of teachers: " << endl << endl;
     for (int i = 0; i < tch_index; i++)
     {
-        showTeacher(teams, teachers, i, numberTeams);
+        showTeacher(teams, teachers, i);
         cout << endl << endl;
     }
 }
 
 void enterTeacher(TEAM* teams, TEACHER* teachers, int& tch_index)
 {
-    int number,id; 
+    int number, id;
 
     cout << "Enter info about teacher:" << endl;
 
@@ -388,6 +492,8 @@ void enterTeacher(TEAM* teams, TEACHER* teachers, int& tch_index)
 
     cout << "Number of teams they assist: ";
     cin >> number;
+
+    teachers[tch_index].numOfTeams = number;
 
     cout << "Enter the IDs of the teams they assist:" << endl;
     for (int i = 0; i < number; i++)
@@ -410,11 +516,49 @@ void deleteTeacher(TEACHER* teachers, int& tch_index, int position)
     tch_index--;
 }
 
+void updateTeacherName(TEACHER* teachers, int position)
+{
+    cout << "Edit the name of the selected teacher:" << endl;
+    cout << " First name: ";
+    cin >> teachers[position].name;
+    cout << " Last name: ";
+    cin >> teachers[position].surname;
+}
 
+void updateTeacherEmail(TEACHER* teachers, int position)
+{
+    cout << "Edit the e-mail of the selected teacher:" << endl;
+    cout << " New E-mail: ";
+    cin >> teachers[position].email;
+}
+
+void addTeamToTeacher(TEAM* teams, TEACHER* teachers, int position)
+{
+    int id;
+    cout << "Enter the ID number of the team you want to be assisted by the selected tacher: ";
+    cin >> id;
+
+    teachers[position].teams[teachers[position].numOfTeams] = teams[id - 1];
+    teachers[position].numOfTeams++;
+}
+
+void removeTeamFromTeacher(TEAM* teams, TEACHER* teachers, int tch_index, int position)
+{
+    int numberOfTeams = teachers[tch_index].numOfTeams;
+
+    for (int i = position; i < numberOfTeams - 1; i++)
+    {
+        teachers[tch_index].teams[i] = teachers[tch_index].teams[i + 1];
+    }
+
+    teachers[tch_index].numOfTeams--;
+}
+
+/*==========================================================================*/
 
 void greetings()
 {
-    cout << "Hello from us, the codebenders and welcome to out program!"<<endl<<endl;
+    cout << "Hello from us, the codebenders and welcome to out program!" << endl << endl;
 }
 
 int searchByStatus()
@@ -563,7 +707,7 @@ int searchTeamByCriteria()
 
 int updateTeamInfo()
 {
-    cout << "1. Name"<<endl;
+    cout << "1. Name" << endl;
     cout << "2. Description" << endl;
     cout << "3. Status" << endl;
 
@@ -589,10 +733,10 @@ int updateTeamInfo()
     return false;
 }
 
-int searchTeacherByCriteria() 
+int searchTeacherByCriteria()
 {
-    cout << "1. Name"<<endl;
-    cout << "2. Team"<<endl;
+    cout << "1. Name" << endl;
+    cout << "2. Team" << endl;
 
     char userChoice;
     cout << "Enter your choice: ";
@@ -678,11 +822,11 @@ int studentSearchMenu()
 int teamSearchMenu()
 {
     cout << endl;
-    cout << "1. Show a list of all the teams"<<endl;
-    cout << "2. Add a new team"<<endl;
-    cout << "3. Delete a team"<<endl;
-    cout << "4. Search a team by criteria"<<endl;
-    cout << "5. Update info about a team"<<endl;
+    cout << "1. Show a list of all the teams" << endl;
+    cout << "2. Add a new team" << endl;
+    cout << "3. Delete a team" << endl;
+    cout << "4. Search a team by criteria" << endl;
+    cout << "5. Update info about a team" << endl;
 
     char userChoice;
     cout << "Enter your choice: ";
@@ -716,11 +860,11 @@ int teamSearchMenu()
 int teacherSearchMenu()
 {
     cout << endl;
-    cout << "1. Show a list of all the teachers"<<endl;
-    cout << "2. Add a new teacher"<<endl;
-    cout << "3. Delete a teacher"<<endl;
-    cout << "4. Search a teacher by criteria"<<endl;
-    cout << "5. Update info about a teacher"<<endl;
+    cout << "1. Show a list of all the teachers" << endl;
+    cout << "2. Add a new teacher" << endl;
+    cout << "3. Delete a teacher" << endl;
+    cout << "4. Search a teacher by criteria" << endl;
+    cout << "5. Update info about a teacher" << endl;
 
     char userChoice;
     cout << "Enter your choice: ";
@@ -814,8 +958,8 @@ int main()
     showTeams(students, teams, t_index, 3);*/
 
     initTeachers(teams, teachers, tch_index);
-    showTeachers(teams, teachers, tch_index, 2);
+    showTeachers(teams, teachers, tch_index);
     enterTeacher(teams, teachers, tch_index);
     deleteTeacher(teachers, tch_index, 0);
-    showTeachers(teams, teachers, tch_index, 2);
+    showTeachers(teams, teachers, tch_index);
 }
